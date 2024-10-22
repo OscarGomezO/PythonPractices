a090d5487d873577994a5597342e6ac192b0fedb
Oscar Gomez <oscar.gomez@utp.edu.co>
2024-10-22 01:14:37 -0500

    Laboratorio de practica funciones, numeros primos

--- Seccion_9_Funciones.py
+++ Seccion_9_Funciones.py
@@ -596,3 +596,24 @@ print(is_leap_year(year))
 print(f"El mes {month} tiene", days_in_month(month, year), "dias.")
 """
 
+
+
+#NUMEROS PRIMOS
+
+def is_prime(num):
+	if num == 1 or num == 0:
+		return False
+	elif num == 2:
+		return True
+	else:
+		for i in range (2, num):
+			if num % 2 == 0:
+				return False
+			elif num % i == 1 and i == num-1:
+				return True
+
+
+for i in range(1, 20):
+	if is_prime(i):
+		result = is_prime(i)
+		print(i,result, end=" ")
